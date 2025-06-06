9d926556fe0b6ada366170c89a69360c7c23173a
Volker Mauel <volkermauel@gmail.com>
2025-06-06 14:55:29 +0200

    fix: ensure single copy button click

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -2,12 +2,12 @@ describe("Click all tabs in peer modal", () => {
   it("passes", () => {
     cy.visit("/install");
     cy.get("div").contains("Linux").click();
-    cy.get("[data-cy=copy-to-clipboard]").click();
+    cy.get("[data-cy=copy-to-clipboard]").first().click();
     cy.get("div").contains("Windows").click();
-    cy.get("[data-cy=copy-to-clipboard]").click();
+    cy.get("[data-cy=copy-to-clipboard]").first().click();
     cy.get("div").contains("Android").click();
-    cy.get("[data-cy=copy-to-clipboard]").click();
+    cy.get("[data-cy=copy-to-clipboard]").first().click();
     cy.get("div").contains("Docker").click();
-    cy.get("[data-cy=copy-to-clipboard]").click();
+    cy.get("[data-cy=copy-to-clipboard]").first().click();
   });
 });
